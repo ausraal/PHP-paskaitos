08a23f8908abc743c30bd5e4f768b12ff6468e37
ausraal <ausra.al@gmail.com>
2023-01-04 21:06:35 +0200

    submit form and see in localhost

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="POST" action="submit.php">
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" /> <br>
+    <label for="surname">Last name:</label><br>
+    <input type="text" id="surname" name="surname" />
+    <input type="submit" />
+</form>
+
+</body>
+</html>
